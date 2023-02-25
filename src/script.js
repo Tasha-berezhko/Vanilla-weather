424fe0d8587eb45221e1c4af5fa51c54a395a034
Tasha-berezhko <124585701+Tasha-berezhko@users.noreply.github.com>
2023-02-25 21:53:23 +0200

    Deleted console.log

--- src/script.js
+++ src/script.js
@@ -59,7 +59,6 @@ function displayTemperature(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "2ff29bed3181c3526c35cc5408037f85";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(function (response) {
